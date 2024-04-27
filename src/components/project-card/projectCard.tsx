91fc2b5cd288c1b107702a1edd1baaa770674490
Sohaib Meo <sohaibmayo12@gmail.com>
2024-04-28 04:53:26 +0500

    better project section + small animation/transition

---
+++ src/components/project-card/projectCard.tsx
@@ -0,0 +1,64 @@
+import { getSingleProjectData } from "../../utils/getSkillData";
+import Git from "../../assets/code-icons/git.svg";
+import "./projectCard.css";
+
+type Project = {
+  startDate: Date;
+  title: string;
+  description: string;
+  techStack: string[];
+  link: string;
+  github: string;
+};
+
+const ProjectCard = ({
+  project,
+  counter,
+}: {
+  project: Project;
+  counter: number;
+}) => {
+  return (
+    <div
+      className="project-card"
+      onClick={() => window.open(project.link, "_blank")}
+    >
+      <h1 className="counter">{counter}</h1>
+      <div className="project-card-footer">
+        <p className="project-start-date">
+          {project.startDate.toLocaleDateString()}
+        </p>
+        <h1 className="project-card-title">{project.title}</h1>
+        <p className="project-card-description">{project.description}</p>
+        <div className="project-card-links">
+          <a href={project.github} className="project-link" target="_blank">
+            <img
+              src={Git}
+              width={"35px"}
+              style={{ aspectRatio: 1 }}
+              alt="github"
+            />
+            <p>Github</p>
+          </a>
+        </div>
+      </div>
+      <div className="project-card-tech-stack">
+        {project.techStack.map((tech, index) => {
+          const skill = getSingleProjectData(tech);
+          return (
+            <a href={skill?.link} target="_blank" key={index}>
+              <img
+                src={skill?.img}
+                width={"30px"}
+                className="icon-image"
+                alt={skill?.alt}
+              />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -1,65 +1,14 @@
 import "./projects.css";
 import "../common.css";
-import Git from "../../assets/code-icons/git.svg";
 import getProjectData from "../../utils/projectData";
-import { getSingleProjectData } from "../../utils/getSkillData";
+import ProjectCard from "../../components/project-card/projectCard";
 
 const Projects = () => {
   const projectData = getProjectData();
   return (
     <section id="projects" className="section project-wrapper">
       {projectData.map((project, index) => {
-        return (
-          <div className="project-card" key={index}>
-            <div className="project-card-header">
-              <p className="project-start-date">{project.startDate.toLocaleDateString()}</p>
-              <h3 className="project-card-title">{project.title}</h3>
-            </div>
-            <div className="project-card-body">
-              <p className="project-card-description">{project.description}</p>
-              <div className="project-card-tech-stack">
-                {project.techStack.map((tech, index) => {
-                  const skill = getSingleProjectData(tech);
-                  return (
-                    <a href={skill?.link} target="_blank" key={index}>
-                      <img
-                        src={skill?.img}
-                        width={"30px"}
-                        className="icon-image"
-                        alt={skill?.alt}
-                      />
-                    </a>
-                  );
-                })}
-              </div>
-              <div className="project-card-footer">
-                <div>
-                  <a
-                    href={project.link}
-                    className="project-link"
-                    target="_blank"
-                  >
-                    <button>View Project</button>
-                  </a>
-                </div>
-                <div>
-                  <a
-                    href={project.github}
-                    className="project-link"
-                    target="_blank"
-                  >
-                    <img
-                      src={Git}
-                      width={"35px"}
-                      style={{ aspectRatio: 1 }}
-                      alt="github"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
+        return <ProjectCard project={project} counter={index+1} key={index} />;
       })}
     </section>
   );
