7fa4a218b91ebf3d3fcb78ceb52cdd2708d7ef36
Sohaib Meo <sohaibmayo12@gmail.com>
2024-04-30 16:35:19 +0500

    responsive eye movement home page

--- src/sections/moving-face/Face.tsx
+++ src/sections/moving-face/Face.tsx
@@ -2,10 +2,7 @@ import "./Face.css";
 import "../common.css";
 import Hair from "../../assets/pieces/hair.svg";
 import Beard from "../../assets/pieces/lower_head.svg";
-import LeftEye from "../../assets/pieces/left_eye.svg";
-import RightEye from "../../assets/pieces/right_eye.svg";
 import DragonEyes from "../../assets/pieces/dragon_eyes.svg";
-// import Full from "../../assets/pieces/blackwhite.svg";
 import { useEffect, useState } from "react";
 
 const Face = () => {
@@ -13,8 +10,9 @@ const Face = () => {
 
   useEffect(() => {
     const handleMouseMove = (event: any) => {
-      const posX = event.clientX - window.innerWidth / 2;
-      const posY = event.clientY - window.innerHeight / 2;
+      const posX = (event.clientX - window.innerWidth / 2) / window.innerWidth;
+      const posY =
+        (event.clientY - window.innerHeight / 2) / window.innerHeight;
       setPosition({ x: posX, y: posY });
     };
 
@@ -31,43 +29,24 @@ const Face = () => {
         <img src={Hair} alt="hair" className="hair" />
         <img src={DragonEyes} alt="eyes" className="eyes" />
         <div className="eyes-container">
-          <div className="left-eye-container">
+          <div className="left-eye">
             <div
-              className="left-eyeball"
+              className="left-eyeball-shine"
               style={{
-                transform: `translate(${position.x * 0.01}%, ${position.y * 0.01}%)`,
+                transform: `translate(${position.x * 100 * 2.5}%, ${position.y * 100 * 1.5}%)`,
               }}
-            >
-              <div
-                className="left-eyeball-shine"
-                style={{
-                  transform: `translate(${position.x * 0.08}%, ${position.y * 0.08}%)`,
-                }}
-              ></div>
-            </div>
+            ></div>
           </div>
-          <div className="right-eye-container">
+          <div className="right-eye">
             <div
-              className="right-eyeball"
+              className="right-eyeball-shine"
               style={{
-                transform: `translate(${position.x * 0.01}%, ${position.y * 0.01}%)`,
+                transform: `translate(${position.x * 100 * 2.5}%, ${position.y * 100 * 1.5}%)`,
               }}
-            >
-              <div
-                className="right-eyeball-shine"
-                style={{
-                  transform: `translate(${position.x * 0.08}%, ${position.y * 0.08}%)`,
-                }}
-              ></div>
-            </div>
+            ></div>
           </div>
-        </div>
-        <div className="eye-container">
-          <img src={LeftEye} alt="left eye" className="left-eye" />
-          <img src={RightEye} alt="right eye" className="right-eye" />
         </div>
         <img src={Beard} alt="beard" className="beard" />
-        {/* <img src={Full} alt="full" className="full" /> */}
       </div>
     </section>
   );
