ddc19d15cd07db8b80e5cb08c2cdb649958c7343
Sohaib Meo <sohaibmayo12@gmail.com>
2024-05-03 01:15:54 +0500

    more images for parallex image movement

--- src/sections/parallex/Parallex.tsx
+++ src/sections/parallex/Parallex.tsx
@@ -2,12 +2,12 @@ import './Parallex.css'
 
 const Parallex = () => {
   return (
-    <section className="section parallex-container">
-      <div className=" bg1" />
-      <div className=" bg2" />
-      <div className=" bg3" />
-      <div className=" bg4" />
-      <div className=" text-layer">
+    <section className="parallex-container">
+      <div className="bg1" />
+      <div className="bg2" />
+      <div className="bg3" />
+      <div className="bg4" />
+      <div className="text-layer">
        <h1>Parallex</h1>
       </div>
     </section>
