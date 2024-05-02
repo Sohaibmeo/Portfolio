822daeede4049a14084de7eccd964967e138bf99
Sohaib Meo <sohaibmayo12@gmail.com>
2024-05-02 14:21:35 +0500

    fixed navigation #home

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     <div className="navbar-wrapper">
       <div className="navbar">
         <div>
-          <a href="#home">Sohaib Meo</a>
+          <a href="#">Sohaib Meo</a>
         </div>
         <div className="navbar-links">
-          <a href="#home">Home</a>
+          <a href="#">Home</a>
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
