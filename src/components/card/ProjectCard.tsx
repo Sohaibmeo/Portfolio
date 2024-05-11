a040191ee1e402365c4c8e23b525935178298e93
Sohaib Meo <sohaibmayo12@gmail.com>
2024-05-11 18:34:38 +0500

    better user experience on project page

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -3,18 +3,21 @@ import { ProjectCardProps } from "../../utils/types/project";
 import Git from "../../assets/code-icons/git.svg";
 import "./ProjectCard.css";
 import { motion, useTransform } from "framer-motion";
+import LinkIcon from "../../assets/link.svg";
+import { useCursor } from "../../contexts/Cursor";
 
 const ProjectCard = ({
   title,
   description,
   background,
-  // link,
+  link,
   github,
   techStack,
   tag,
   startDate,
   scrollYProgress,
 }: ProjectCardProps) => {
+  const { toggleShow, setCursorContent, setCursorSize } = useCursor();
   const y = useTransform(
     scrollYProgress,
     [0, 0.5, 1],
@@ -25,12 +28,25 @@ const ProjectCard = ({
     [0, 0.5, 0.99, 1],
     [1, 1, 1, 0],
   );
+  const changeCursorLook = () => {
+    toggleShow();
+    setCursorContent("view project");
+    setCursorSize(80);
+  };
+  const resetCursorLook = () => {
+    toggleShow();
+    setCursorContent("");
+    setCursorSize(10);
+  };
   return (
-    <motion.div
-      className="project-card"
-      // onClick={() => window.open(link, "_blank")}
-      style={{ backgroundImage: `url(${background})`, y, opacity }}
-    >
+    <motion.div className="project-card" style={{ y, opacity }}>
+      <div
+        className="project-background-img"
+        onClick={() => window.open(link, "_blank")}
+        onMouseEnter={changeCursorLook}
+        onMouseLeave={resetCursorLook}
+        style={{ backgroundImage: `url(${background})` }}
+      />
       <div className="project-card-tag">
         <p>{tag}</p>
       </div>
@@ -38,7 +54,18 @@ const ProjectCard = ({
         <p className="project-start-date">{startDate.toLocaleDateString()}</p>
         <h2 className="project-card-title">{title}</h2>
         <p className="project-card-description">{description}</p>
-        <div className="project-card-links">
+        <div className="project-card-links" >
+          <a href={link} className="project-link" target="_blank">
+            <p>View</p>
+            <img
+              loading="eager"
+              src={LinkIcon}
+              width={"18px"}
+              style={{ aspectRatio: 1 }}
+              color="white"
+              alt="Link"
+            />
+          </a>
           <a href={github} className="project-link" target="_blank">
             <img
               loading="eager"
@@ -47,7 +74,14 @@ const ProjectCard = ({
               style={{ aspectRatio: 1 }}
               alt="github"
             />
-            <p>Github</p>
+            <p>Code</p>
+            <img
+              loading="eager"
+              src={LinkIcon}
+              width={"18px"}
+              style={{ aspectRatio: 1 }}
+              alt="Link"
+            />
           </a>
         </div>
       </div>
